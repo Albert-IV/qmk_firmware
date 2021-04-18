e9b0513fd04f37234979524396f0b41a1c32b418
Albert Engelbrecht <dropped.on.japan@gmail.com>
2021-04-18 00:39:01 -0500

    WIP keymap based off my old kyria layout.

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
@@ -1,28 +1,11 @@
 #include QMK_KEYBOARD_H
 
-
-#define _BASE 0
-#define _RAISE 1
-#define _LOWER 2
-
-// Fillers to make layering more clear
-
-#define ____ KC_TRNS
-
-#define SFT_ESC  SFT_T(KC_ESC)
-#define CTL_BSPC CTL_T(KC_BSPC)
-#define ALT_SPC  ALT_T(KC_SPC)
-#define SFT_ENT  SFT_T(KC_ENT)
-
-#define KC_ML KC_MS_LEFT
-#define KC_MR KC_MS_RIGHT
-#define KC_MU KC_MS_UP
-#define KC_MD KC_MS_DOWN
-#define KC_MB1 KC_MS_BTN1
-#define KC_MB2 KC_MS_BTN1
-
-#define RAISE MO(_RAISE)
-#define LOWER MO(_LOWER)
+enum layers {
+    _QWERTY = 0,
+    _LOWER,
+    _RAISE,
+    _ADJUST
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -44,39 +27,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                           +-------------+ +-------------+
  */
 
-[_BASE] = LAYOUT( \
-    KC_ESC,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_MINS,   \
-    KC_TAB,  KC_A,  KC_S,   KC_D,   KC_F,   KC_G,               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,   \
-    KC_LSFT, KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_BSLASH, \
-                    KC_LBRC,KC_RBRC,                                            KC_PLUS,KC_EQL,                    \
-                                    KC_SPC,                             LOWER,                                     \
-                                    KC_TAB, KC_HOME,            KC_END, KC_DEL,                                    \
-                                    KC_BSPC,KC_GRV,             KC_LGUI,KC_LALT                                    \
+[_QWERTY] = LAYOUT( \
+    KC_ESC,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,                     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSLASH, \
+    KC_LSFT, KC_A,  KC_S,   KC_D,   KC_F,   KC_G,                     KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,   \
+    KC_LSFT, KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,                     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_BSLASH, \
+                    KC_LCBR,KC_LPRN,                                                  KC_RPRN,KC_RCBR,                   \
+                                                                                                                         \
+                            KC_LALT,                                                  KC_LSFT,                           \
+                            KC_LGUI,LT(_LOWER, KC_BSPACE),         LT(_LOWER, KC_TAB),KC_SPC,                            \
+                            KC_HOME,LT(_RAISE, KC_DELETE),         LT(_LOWER, KC_TAB),KC_END                             \
 ),
 
 [_LOWER] = LAYOUT(
-    _______,_______,_______,_______,_______,KC_LBRC,            KC_RBRC, KC_P7, KC_P8,  KC_P9,  RESET,  KC_PLUS,   \
-    _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,            KC_RPRN, KC_P4, KC_P5,  KC_P6,  KC_MINS,KC_PIPE,   \
-    _______,_______,_______,_______,_______,_______,            _______, KC_P1, KC_P2,  KC_P3,  KC_EQL, KC_UNDS,   \
-                                            _______,KC_PSCR,    _______, KC_P0,                                    \
+    _______,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,            _______,_______,_______,_______,_______,KC_BSLS,   \
+    _______,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,             KC_PLUS,KC_MINS,KC_SLSH,KC_ASTR,KC_PERC,KC_QUOT,   \
+    _______,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,            KC_AMPR,KC_EQL, KC_COMM,KC_DOT, KC_SLSH,KC_MINS,   \
+                    _______,_______,                                            _______,_______,                   \
                                             _______,                    _______,                                   \
-                                            _______,_______,    _______,_______,                                   \
+                                            KC_SCLN,KC_EQL,     KC_EQL, KC_SCLN,                                   \
                                             _______,_______,    _______,_______                                    \
 ),
 
 [_RAISE] = LAYOUT(
-    _______,RESET,  _______,_______,_______,KC_LBRC,            KC_RBRC,_______,KC_NLCK,KC_INS, KC_SLCK,KC_MUTE,   \
-    _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,            KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,   \
-    _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,KC_VOLD,   \
-                                            _______,_______,    KC_EQL, _______,                                   \
+    _______,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,               KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   _______,   \
+    _______,_______,_______,_______,_______,KC_VOLU,            KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,_______,_______,   \
+    _______,_______,_______,_______,KC_MUTE,KC_VOLD,            KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,_______,_______,   \
+                    _______,_______,                                            _______, _______,                  \
                                             _______,                    _______,                                   \
                                             _______,_______,    _______,_______,                                   \
                                             _______,_______,    _______,_______                                    \
-)
+),
+
+
+[_ADJUST] = LAYOUT(
+    _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,              KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, _______,   \
+    _______,_______,_______,_______,_______,_______,            _______,_______,_______,KC_F11, KC_F12, _______,   \
+    _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,_______,   \
+                    _______,_______,                                            _______, _______,                  \
+                                            _______,                    _______,                                   \
+                                            _______,_______,    _______,_______,                                   \
+                                            _______,_______,    _______,_______                                    \
+),
 };
 
-
-void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
+
+// void persistent_default_layer_set(uint16_t default_layer) {
+//   eeconfig_update_default_layer(default_layer);
+//   default_layer_set(default_layer);
+// }
