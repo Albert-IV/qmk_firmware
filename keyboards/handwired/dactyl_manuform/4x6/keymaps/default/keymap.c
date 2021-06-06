3c651148c4d08c8f39ca490ee0e08bd412a68c89
Albert Engelbrecht <me@albertengelbrecht.com>
2021-06-05 22:55:44 -0500

    Added more base layers?  Probably?

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
@@ -1,5 +1,12 @@
 #include QMK_KEYBOARD_H
 
+enum base_layers {
+  _QWERTY,
+  _WINDOWS_QWERTY,
+  _COLEMAK_DH,
+  _GAMING_QWERTY
+}
+
 enum layers {
     _QWERTY = 0,
     _LOWER,
@@ -51,22 +58,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT(
     _______,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,               KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   _______,   \
     _______,_______,_______,_______,_______,KC_VOLU,            KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,_______,_______,   \
-    _______,_______,_______,_______,KC_MUTE,KC_VOLD,            KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,_______,_______,   \
+    _______,_______,_______,_______,KC_MUTE,KC_VOLD,            _______,_______,_______,_______,_______,_______,   \
                     _______,_______,                                            _______, _______,                  \
                                             _______,                    _______,                                   \
                                             _______,_______,    _______,_______,                                   \
                                             _______,_______,    _______,_______                                    \
 ),
 
-
 [_ADJUST] = LAYOUT(
     _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,              KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, _______,   \
     _______,_______,_______,_______,_______,_______,            _______,_______,_______,KC_F11, KC_F12, _______,   \
-    _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,_______,   \
+    _______,_______,_______,_______,_______,_______,            DF(_QWERTY),DF(_WINDOWS_QWERTY),DF(_COLEMAK_DH),DF(_GAMING_QWERTY),_______,_______,  \
                     _______,_______,                                            _______, _______,                  \
                                             _______,                    _______,                                   \
                                             _______,_______,    _______,_______,                                   \
                                             _______,_______,    _______,_______                                    \
+),
+
+[_WINDOWS_QWERTY] = LAYOUT( \
+    KC_ESC,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,                     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSLASH, \
+    KC_LSFT, KC_A,  KC_S,   KC_D,   KC_F,   KC_G,                     KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,   \
+    KC_LGUI, KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,                     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_MINS,   \
+                    KC_LCBR,KC_LPRN,                                                  KC_RPRN,KC_RCBR,                   \
+                                                                                                                         \
+                            KC_LALT,                                                  KC_LSFT,                           \
+                            KC_LCTL,LT(_LOWER, KC_BSPACE),         LT(_RAISE, KC_ENT),KC_SPC,                            \
+                            KC_HOME,LT(_RAISE, KC_DELETE),         LT(_LOWER, KC_TAB),KC_END                             \
+),
+
+[_COLEMAK_DH] = LAYOUT( \
+    KC_ESC,  KC_Q,  KC_W,   KC_F,   KC_P,   KC_B,                     KC_J,   KC_L,   KC_U,   KC_Y,   KC_SCLN,KC_BSLASH, \
+    KC_LSFT, KC_A,  KC_R,   KC_S,   KC_T,   KC_G,                     KC_M,   KC_N,   KC_E,   KC_I,   KC_O,   KC_QUOT,   \
+    KC_LCTL, KC_X,  KC_C,   KC_D,   KC_V,   KC_Z,                     KC_K,   KC_H,   KC_COMM,KC_DOT, KC_SLSH,KC_MINS,   \
+                    KC_LCBR,KC_LPRN,                                                  KC_RPRN,KC_RCBR,                   \
+                                                                                                                         \
+                            KC_LALT,                                                  KC_LSFT,                           \
+                            KC_LGUI,LT(_LOWER, KC_BSPACE),         LT(_RAISE, KC_ENT),KC_SPC,                            \
+                            KC_HOME,LT(_RAISE, KC_DELETE),         LT(_LOWER, KC_TAB),KC_END                             \
+),
+
+[_GAMING_QWERTY] = LAYOUT( \
+    KC_ESC,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,                     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSLASH, \
+    KC_LSFT, KC_A,  KC_S,   KC_D,   KC_F,   KC_G,                     KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,   \
+    KC_LCTL, KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,                     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_MINS,   \
+                    KC_LCBR,KC_LPRN,                                                  KC_RPRN,KC_RCBR,                   \
+                                                                                                                         \
+                            KC_LALT,                                                  KC_LSFT,                           \
+                            KC_SPC, KC_BSPACE,                     LT(_RAISE, KC_ENT),KC_SPC,                            \
+                            KC_HOME,KC_DELETE,                     LT(_LOWER, KC_TAB),KC_END                             \
 ),
 };
 
@@ -74,7 +113,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
-// void persistent_default_layer_set(uint16_t default_layer) {
-//   eeconfig_update_default_layer(default_layer);
-//   default_layer_set(default_layer);
-// }
+void base_layer_up() {
+}
+
