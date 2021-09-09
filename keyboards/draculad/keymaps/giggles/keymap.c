d7cf47b1d358691a864bf6b217a8ca99efba5323
Albert Engelbrecht <me@albertengelbrecht.com>
2021-09-09 11:06:33 -0500

    Add modifiers to the num layer

--- keyboards/draculad/keymaps/giggles/keymap.c
+++ keyboards/draculad/keymaps/giggles/keymap.c
@@ -50,14 +50,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_SYMB] = LAYOUT(
         KC_EXLM,         KC_AT,   KC_GRV,  KC_PLUS,         KC_SLSH,                       KC_BSLS, KC_AMPR,         KC_TILD, KC_EQL,  KC_PIPE,
         LSFT_T(KC_HASH), KC_DLR,  KC_LPRN, KC_LCBR,         KC_LBRC,                       KC_RBRC, KC_RCBR,         KC_RPRN, KC_PERC, RSFT_T(KC_QUOT),
-        KC_LCTL,         XXXXXXX, KC_LALT, LGUI_T(KC_CIRC), KC_UNDS,                       KC_MINUS, RGUI_T(KC_ASTR), KC_RALT, XXXXXXX, KC_RCTL,
+        KC_LCTL,         XXXXXXX, KC_LALT, LGUI_T(KC_CIRC), KC_UNDS,                       KC_MINUS, RGUI_T(KC_ASTR), KC_RALT, XXXXXXX,KC_RCTL,
                                                             XXXXXXX,                       XXXXXXX,
                                                    XXXXXXX, XXXXXXX, XXXXXXX,     LT(_ADJ, KC_TAB), XXXXXXX, XXXXXXX
     ),
     [_NUM] = LAYOUT(
-        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLU,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD,                      KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_RSFT,
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,            KC_8,            KC_9,    KC_0,
+        KC_LSFT, XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLU,                      KC_LEFT, KC_DOWN,         KC_UP,           KC_RGHT, KC_RSFT,
+        KC_LCTL, XXXXXXX, KC_LALT, KC_LGUI, KC_VOLD,                      KC_HOME, RGUI_T(KC_PGDN), RALT_T(KC_PGUP), KC_END,  KC_RCTL,
                                             XXXXXXX,                      XXXXXXX,
                                 XXXXXXX, XXXXXXX, LT(_ADJ, KC_DELETE),    XXXXXXX, XXXXXXX, XXXXXXX
     ),
